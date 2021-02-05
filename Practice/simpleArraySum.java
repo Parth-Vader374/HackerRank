69ca3559d19f11f3f288098e93ae9387f2aadb42
Mehta <Parth.Mehta@broadridge.com>
2021-02-05 16:58:58 -0500

    Practice

---
+++ Practice/simpleArraySum.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class simpleArraySum {
+    static int simpleArray(int[] ar) {
+        int sum = 0;
+        for (int i = 0; i < ar.length; i++) {
+            sum += ar[i];
+        }
+        return sum;
+    }
+}
